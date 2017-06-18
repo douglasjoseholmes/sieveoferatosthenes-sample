ebdf0d36881af60b85cb4f2768b3dfc2e64c33b5
Douglas Holmes <douglasjoseholmes@gmail.com>
2017-06-18 15:35:00 -0700

    - Added UI, Renamed away from "HelloWorld"

--- SieveOfEratosthenesUWP/MainPage.xaml.cs
+++ SieveOfEratosthenesUWP/MainPage.xaml.cs
@@ -1,41 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace HelloWorld
+namespace SieveOfEratosthenesUWP
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
-            testBlock.Text = string.Join("|", new Sieve(1000).Solve().ToArray());
+        }
+
+        private void btnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            testBlock.Text = string.Join("|", new Sieve(int.Parse(txtMin.Text), int.Parse(txtMax.Text)).Solve().ToArray());
         }
 
         public class Sieve
         {
             public int Minimum { get; set; }
             public int Maximum { get; set; }
-            
+
             public Sieve(int max)
             {
                 Minimum = 0;
+                Maximum = max;
+            }
+
+            public Sieve(int min, int max)
+            {
+                Minimum = min;
                 Maximum = max;
             }
 
@@ -43,7 +41,6 @@ namespace HelloWorld
             {
                 HashSet<int> composite = new HashSet<int>();
                 var primes = new List<string>();
-                long count = 0;
                 for (int x = 2; x < Maximum; x++)
                 {
                     for (int y = x * 2; y < Maximum; y = y + x)
@@ -58,16 +55,13 @@ namespace HelloWorld
 
                 }
 
-                for (int z = 2; z < Maximum; z++)
+                for (int z = Minimum; z < Maximum; z++)
                 {
                     if (!composite.Contains(z))
                     {
                         primes.Add(z.ToString());
-                        count = count + z;
                     }
                 }
-
-                Console.WriteLine("Sum is: " + count);
                 return primes;
             }
         }
